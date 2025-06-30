ab1bdc9e2186242afc7ef2debccea3c6c757dd6c
kamiye <kamiye31@gmail.com>
2025-06-30 12:28:02 +0100

    variables for text content

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
+  );
 }
 
 export default App
