901e079b6461990ca97584f195485e32b98a666b
kamiye <kamiye31@gmail.com>
2025-06-30 12:33:41 +0100

    Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
