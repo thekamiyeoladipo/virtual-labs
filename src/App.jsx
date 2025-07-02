2dec03b95b151f620b0eed8b19a111f1a69e6547
kamiye <kamiye31@gmail.com>
2025-07-02 15:06:03 +0100

    Add Features component and integrate it into the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Features from './components/Features';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div className='max-w-7xl mx-auto px-6 pt-20'>
         <Hero />
       </div>
+      <Features />
     </>
   );
 }
