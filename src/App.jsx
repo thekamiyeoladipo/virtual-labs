3215027f578a583677a459ff47838c1937e5d61f
kamiye <kamiye31@gmail.com>
2025-07-02 14:43:51 +0100

    Add Hero component to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className='max-w-7xl mx-auto px-6 pt-20'>
+        <Hero />
+      </div>
     </>
   );
 }
