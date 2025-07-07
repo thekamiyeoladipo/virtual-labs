8113f46a71e24900c2b36e65349acba8cad2deae
kamiye <kamiye31@gmail.com>
2025-07-07 17:17:57 +0100

    Refactor Pricing component layout for improved readability and structure

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,20 @@ const Pricing = () => {
   return (
     <div className="mt-20">
         <h2 className="text-3xl sm:text-5xl lg:text-6xl
-        text-center my-8 tracking-wider">Pricing</h2>
+        text-center my-8 tracking-wider">Pricing
+        </h2>
+        <div className="flex flex-wrap">
+            {pricingOptions.map((option, index) => (
+                <div key={index} className="w-full sm:w-1/2
+                lg:w-1/3 p-2">
+                    <div className="p-10 border-neutral-700 rounded-xl">
+                    <p className="text-4xl">
+                        {option.title}
+                    </p>
+                    </div>
+                </div>
+            ))}
+        </div>  
     </div>
   )
 }
