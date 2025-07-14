e450c5fa403a84cfa6799dd8328d931e2805b63c
kamiye <kamiye31@gmail.com>
2025-07-14 13:47:03 +0100

    Refactor Pricing component layout by adjusting padding and margin for improved aesthetics

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -11,7 +11,6 @@ const Pricing = () => {
             {pricingOptions.map((option, index) => (
                 <div key={index} className="flex items-center justify-center w-full sm:w-1/2
                 lg:w-1/3 p-2">
-<<<<<<< HEAD
                     <div className="p-20 border rounded-xl">
                     <p className="text-4xl">
                         {option.title}
@@ -19,15 +18,6 @@ const Pricing = () => {
                             <span className="bg-gradient-to-r from-orange-500 to-red-400
                             text-transparent bg-clip-text text-xl mb-4 ml-2">
                                 (Most Popular)
-=======
-                    <div className="p-10 border-neutral-700 rounded-xl">
-                    <p className="text-4xl mb-8">
-                        {option.title}
-                        {option.title === "Pro" && (
-                            <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent
-                            bg-clip-text text-xl mb-4 ml-2">
-                             (Most popular)   
->>>>>>> d165090a0cfbd8d45b0fd34287764f29ce8c6a6f
                             </span>
                         )}
                     </p>
